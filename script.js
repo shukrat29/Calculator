b39817ef2c54c55e7fe05ab8a204c86000a0e423
shukra <shukratamang.npl@gmail.com>
2024-05-25 20:59:56 +1000

    display string to calculator screen

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const allButtonsElm = document.querySelectorAll('.btn');
 
 let strToDisplay = '';
+const displayElm = document.querySelector('.display');
+
+const buttonAction = (value) => {
+  strToDisplay += value;
+  display(strToDisplay);
+};
 
 allButtonsElm.forEach((btn) => {
   btn.addEventListener('click', () => {
     const value = btn.innerText;
-    strToDisplay += value;
-    console.log(strToDisplay);
+    buttonAction(value);
   });
 });
+
+const display = (str) => {
+  displayElm.innerText = str;
+};
